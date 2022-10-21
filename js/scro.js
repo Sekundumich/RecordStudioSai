f4fd6cd5f51dfc53a9dd6c45f1aca998e0c87b49
Sekundumich <116390825+Sekundumich@users.noreply.github.com>
2022-10-22 00:17:13 +0300

    Sait

---
+++ js/scro.js
@@ -0,0 +1,22 @@
+$(function() {
+
+    var header = $("#header")
+    var introH = $("#intro").innerHeight()
+    var scrollOffset = 0;
+
+    $(window).on("scroll", function(){
+
+
+        scrollOffset = $(this).scrollTop();
+        
+        if( scrollOffset >= introH ){
+            header.addClass("fixed");
+        } else
+        {
+            header.removeClass("fixed");
+        }
+
+    }); 
+
+
+});
